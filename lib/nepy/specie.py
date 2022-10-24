e8212414999c980ea571a829cbbf43d585eea9b5
Çağan Kiper <kipercagan@gmail.com>
2022-10-24 14:40:45 +0300

    refactor: Abort specie

--- lib/nepy/specie.py
+++
@@ -1,9 +0,0 @@
-class Specie:
-    def __init__(self, species_id, member_count, allowed_offspring_count):
-        self.species_id = species_id
-        self.member_count = member_count
-        self.allowed_offspring_count = allowed_offspring_count
-        self.average_fitness = 0
-        self.average_adjusted_fitness = 0
-        self.total_fitness = 0
-        self.generations_since_improved = 0
